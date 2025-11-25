2c29672e8caa54d197f37ce1f08e91b779891dd8
Ubuntu <ubuntu@ip-172-31-0-12.ap-south-1.compute.internal>
2025-11-25 08:08:38 +0000

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let todos = [
+  { id: 1, title: "Learn Jenkins" },
+  { id: 2, title: "Practice Docker" }
+];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const todo = { id: todos.length + 1, title: req.body.title };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.listen(3000, () => {
+  console.log("Todo API running on port 3000");
+});
